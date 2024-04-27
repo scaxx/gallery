c524e33eb0fc5595417c0cb3d4ba3faf2b692ce7
scaxx <153398282+scaxx@users.noreply.github.com>
2024-04-27 20:05:34 -0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,17 @@
+function upDate(previewPic){
+
+    var imageUrl = "url('" + previewPic.src + "')";
+
+    document.getElementById('image').style.backgroundImage = imageUrl;
+
+    document.getElementById('image').innerHTML = previewPic.alt;
+
+}
+
+function unDo(){
+    
+    document.getElementById('image').style.backgroundImage = "url('')";
+    
+    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+
+}
